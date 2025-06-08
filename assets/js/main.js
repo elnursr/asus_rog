da700aa18c6fb86f82cc9553cf2bb407f9fdbe03
elnursr <elnursultanv7@gmail.com>
2025-06-08 21:44:24 +0400

    completed footer partially

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,25 +13,85 @@ sliderWrapper.addEventListener('click', function (e) {
     }
 });
 
-let url = '../assets/data/json/products.json';
+let productsUrl = '../assets/data/json/products.json',
+    bannersUrl = '../assets/data/json/banners.json',
+    socialsUrl = '../assets/data/json/socials.json';
 
-let productList = document.querySelector('.product__list');
+let productList = document.querySelector('.product__list'),
+    footerSocial = document.querySelector('.footer__social'),
+    bannerSlideshow = document.querySelector('.banner-slideshow');
 
-fetch(url)
+fetch(productsUrl)
     .then(res => res.json())
     .then(({ products }) => {
-        products.forEach(product => {
+        products.forEach(({ title }) => {
 
             productList.innerHTML +=
                 `
                     <li class="product__item">
-                        <a href="https://rog.asus.com/${product.title.replace(/_/g, '-').replace(/&/g, '').replace(/--+/g, '-')}-group" class="product__link" target="_blank">
+                        <a href="https://rog.asus.com/${title.replace(/_/g, '-').replace(/&/g, '').replace(/--+/g, '-')}-group" class="product__link" target="_blank">
                             <div class="product__image">
-                                <img src="assets/media/products/${product.title}.png" alt="${product.image_name}">
+                                <img src="assets/media/products/${title}.png" alt="${title}">
                             </div>
-                            <h3 class="product__title">${product.title.replace(/_/g, ' ')}</h3>
+                            <h3 class="product__title">${title.replace(/_/g, ' ')}</h3>
                         </a>
                     </li>
                 `;
         });
+    })
+
+fetch(socialsUrl)
+    .then(res => res.json())
+    .then(({ socials }) => {
+        socials.forEach(({ url, className }) => {
+
+            footerSocial.innerHTML +=
+                `
+                    <li class="footer__social-item">
+                        <a href="${url}" class="footer__social-link" target="_blank">
+                            <i class="fa-brands ${className}"></i>
+                        </a>
+                    </li>
+                `;
+        });
+    })
+
+fetch(bannersUrl)
+    .then(res => res.json())
+    .then(({ banners }) => {
+
+        for (let i = 0; i < banners.length; i++) {
+
+            if (i === 0) {
+                bannerSlideshow.innerHTML +=
+                    `
+                    <li class="slider__item active banner-slideshow__item">
+                        <a href="${banners[i].url}" class="banner-slideshow__link">
+                            <img src="assets/media/banner/${banners[i].image_name}.webp" alt="">
+                        </a>
+                    </li>
+                `;
+            }
+
+            else if (i === (banners.length - 1)) {
+                bannerSlideshow.innerHTML +=
+                    `
+                    <li class="slider__item banner-slideshow__item" data-last-slide="lastImage">
+                        <a href="${banners[i].url}" class="banner-slideshow__link">
+                            <img src="assets/media/banner/${banners[i].image_name}.webp" alt="">
+                        </a>
+                    </li>
+                `;
+            }
+            else {
+                bannerSlideshow.innerHTML +=
+                    `
+                    <li class="slider__item banner-slideshow__item">
+                        <a href="${banners[i].url}" class="banner-slideshow__link">
+                            <img src="assets/media/banner/${banners[i].image_name}.webp" alt="">
+                        </a>
+                    </li>
+                `;
+            }
+        }
     })
